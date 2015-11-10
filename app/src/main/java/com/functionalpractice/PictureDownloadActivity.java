11d0d02e7c2e3dcfbaf3d3dda9bfbaccc5294a06
nichol <zhang.e.ong@gmail.com>
2015-11-10 10:51:19 +0800

    test image loader

--- app/src/main/java/com/functionalpractice/PictureDownloadActivity.java
+++ app/src/main/java/com/functionalpractice/PictureDownloadActivity.java
@@ -1,8 +1,12 @@
 package com.functionalpractice;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 
 /**
@@ -11,9 +15,24 @@ import android.widget.ImageView;
 public class PictureDownloadActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private DisplayImageOptions options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_download_layout);
         imageView = (ImageView)findViewById(R.id.download_image);
+        String url = "http://i.imgur.com/7spzG.png";
+        options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.mipmap.ic_stub)
+                .showImageForEmptyUri(R.mipmap.ic_empty)
+                .showImageOnFail(R.mipmap.ic_error)
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .considerExifParams(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+
+        ImageLoader.getInstance().displayImage(url,imageView,options);
+
     }
 }
